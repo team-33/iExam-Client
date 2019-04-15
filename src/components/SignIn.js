6ef786115a4ef8bcd3032abc582f37a03b57f95c
hirantha <sahanhirantha@gmail.com>
2019-04-15 16:41:49 +0530

    bootstrap react-router-dom and simple routing ui created

---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SignIn extends React.Component {
+    render() {
+        return(
+            <div>
+                sign in
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route} from "react-router-dom";
+
+import App from './components/App';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/signup" component={SignUp}/>
+            <Route exact path="/signin" component={SignIn}/>
+            <Route exact path="/dashboard" component={Dashboard}/>
+        </App>
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
