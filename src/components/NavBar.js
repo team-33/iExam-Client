53e6d970720e07bf15b56924ff395022939385b9
hirantha <sahanhirantha@gmail.com>
2019-04-16 10:40:11 +0530

    Ui changed Bootstrap to react Material

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,75 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default class NavBar extends React.Component {
 
     render() {
         return (
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-                <Link className="navbar-brand" to="/">iExamer</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-                        </li>
-                    </ul>
-                    <ul className="nav navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/signup">Sign up</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/signin">Sign in</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/signout">Sign out</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div style={{flexGrow:1,background:"#111111"}}>
+              <AppBar position='static'>
+                <Toolbar>
+                  <IconButton color='inherit' aria-label="Menu" style={{marginLeft: -20,marginRight: 0}}>
+                    <MenuIcon/>
+                  </IconButton>
+                  <Typography  color='inherit' variant="h5">
+                    <Link className="navbar-brand"  to="/" style={{color:'white', textDecoration: 'none' }}>
+                      iExamr
+                    </Link>
+                    </Typography>
+                    <div style={{flexGrow:1,marginLeft:10}}>
+                    <Link className="navbar-brand" to="/dashboard" style={{color:'white', textDecoration: 'none' }}>
+                      Dashboard
+                    </Link>
+                    </div>
+                    <Link className="nav-link" to="/signup" style={{color:'white', textDecoration: 'none' }}>
+                      <Button color='inherit'>
+                      Sign up
+                      </Button>
+                    </Link>
+                    <Link className="nav-link" to="/signin" style={{color:'white', textDecoration: 'none' }}>
+                      <Button color='inherit'>
+                      Sign in
+                      </Button>
+                    </Link>
+                    <Link className="nav-link" to="/signout" style={{color:'white', textDecoration: 'none' }}>
+                      <Button color='inherit'>
+                      Sign out
+                      </Button>
+                    </Link>
+                </Toolbar>
+              </AppBar>
+            </div>
+
+            // <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+            //     <Link className="navbar-brand" to="/">iExamer</Link>
+            //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            //       <span class="navbar-toggler-icon"></span>
+            //     </button>
+            //     <div className="collapse navbar-collapse"  id="navbarNav">
+            //         <ul className="navbar-nav mr-auto">
+            //             <li className="nav-item">
+            //                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            //             </li>
+            //         </ul>
+            //         <ul className="nav navbar-nav ml-auto">
+            //             <li className="nav-item">
+            //                 <Link className="nav-link" to="/signup">Sign up</Link>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <Link className="nav-link" to="/signin">Sign in</Link>
+            //             </li>
+            //             <li className="nav-item">
+            //                 <Link className="nav-link" to="/signout">Sign out</Link>
+            //             </li>
+            //         </ul>
+            //     </div>
+            // </nav>
         );
     }
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import axios from 'axios'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
 import {GOOGLE_SIGN_IN_API} from '../URL'
 
 const responseGoogle = (response) => {
@@ -19,27 +22,49 @@ export default class SignIn extends React.Component {
 
   render() {
       return(
-          <div>
-            <div className="row">
-              <div className="col"></div>
-              <div className="col text-center">
-                <div className="card" style={{width:"18rem",margin:'auto',marginTop:"20px"}}>
-                  <div className="card-body">
-                    <h5 className="card-title">Login With Google</h5>
-                    <p className="card-text">One click login with google.</p>
-                      <GoogleLogin
-                       clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                       buttonText="Login"
-                       onSuccess={responseGoogle}
-                       onFailure={responseGoogle}
-                       cookiePolicy={'single_host_origin'}
-                      />
-                </div>
-                </div>
 
-              </div>
-            </div>
-          </div>
+        <div style={{flexGrow:1}}>
+          <Grid container>
+            <Grid item xs={12} sm={6} style={{padding:10}}>
+            </Grid>
+            <Grid item xs={12} sm={6} style={{padding:10}}>
+              <Paper style={{height:200,textAlign:'center',padding:20}}>
+                <h4>
+                  Sign In using third Party Methods
+                </h4>
+                <GoogleLogin
+                  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                  buttonText="Login"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy={'single_host_origin'}
+                  />
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+
+          // <div style=>
+          //   <div className="row">
+          //     <div className="col"></div>
+          //     <div className="col text-center">
+          //       <div className="card" style={{width:"18rem",margin:'auto',marginTop:"20px"}}>
+          //         <div className="card-body">
+          //           <h5 className="card-title">Login With Google</h5>
+          //           <p className="card-text">One click login with google.</p>
+          //             <GoogleLogin
+          //              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          //              buttonText="Login"
+          //              onSuccess={responseGoogle}
+          //              onFailure={responseGoogle}
+          //              cookiePolicy={'single_host_origin'}
+          //             />
+          //       </div>
+          //       </div>
+
+          //     </div>
+          //   </div>
+          // </div>
       )
   }
 }
