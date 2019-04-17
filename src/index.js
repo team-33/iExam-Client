6b31d202aeec8bec670c394a746e846768998e06
hirantha <sahanhirantha@gmail.com>
2019-04-17 09:30:42 +0530

    going to add react redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import {BrowserRouter, Route} from "react-router-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 import Home from './components/Home';
@@ -8,16 +11,23 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Dashboard from './components/Dashboard';
 import * as serviceWorker from './serviceWorker';
+import authGurd from './components/AuthManager';
+import reducers from './reducers';
+
+localStorage.removeItem('jwt');
+axios.defaults.headers.common['Authorization'] = '';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/signup" component={SignUp}/>
-            <Route exact path="/signin" component={SignIn}/>
-            <Route exact path="/dashboard" component={Dashboard}/>
-        </App>
-    </BrowserRouter>,
+    <Provider store={createStore(reducers,{})}>
+      <BrowserRouter>
+          <App>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/signup" component={SignUp}/>
+              <Route exact path="/signin" component={SignIn}/>
+              <Route exact path="/dashboard" component={authGurd(Dashboard)}/>
+          </App>
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,2 @@
+import { combineReducers } from 'redux';
+import { reducers as formReducers } from 'redux-form';
