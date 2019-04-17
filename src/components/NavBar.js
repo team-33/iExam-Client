c8c222f229b61708236c89b96b6a5bd5b3a8131f
hirantha <sahanhirantha@gmail.com>
2019-04-17 09:28:52 +0530

    Navbar changed littel bit but i cant remember what

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,31 +45,6 @@ export default class NavBar extends React.Component {
                 </Toolbar>
               </AppBar>
             </div>
-
-            // <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-            //     <Link className="navbar-brand" to="/">iExamer</Link>
-            //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            //       <span class="navbar-toggler-icon"></span>
-            //     </button>
-            //     <div className="collapse navbar-collapse"  id="navbarNav">
-            //         <ul className="navbar-nav mr-auto">
-            //             <li className="nav-item">
-            //                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            //             </li>
-            //         </ul>
-            //         <ul className="nav navbar-nav ml-auto">
-            //             <li className="nav-item">
-            //                 <Link className="nav-link" to="/signup">Sign up</Link>
-            //             </li>
-            //             <li className="nav-item">
-            //                 <Link className="nav-link" to="/signin">Sign in</Link>
-            //             </li>
-            //             <li className="nav-item">
-            //                 <Link className="nav-link" to="/signout">Sign out</Link>
-            //             </li>
-            //         </ul>
-            //     </div>
-            // </nav>
         );
     }
 }
