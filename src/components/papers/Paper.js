4a20d937819f8b024da10e23aea10a9bbfd3ef5a
hirantha <sahanhirantha@gmail.com>
2019-04-18 22:55:30 +0530

    current paper state recieved,but i will change it for redux if it needs

--- src/components/papers/Paper.js
+++ src/components/papers/Paper.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import Fade from '@material-ui/core/Fade';
+
+import { GET_PAPER_API } from '../../URL'
 
 class Paper extends React.Component {
 
-  constructor(props) {
+  constructor(props){
     super(props);
+    this.state = {
+      paper:''
+    }
+  }
+
+  componentWillMount() {
+    var { subject , year } = this.props.match.params;
     //TODO: get paper from server
     //paper details and answers
-    this.state = {date: new Date()};
+    axios.get(GET_PAPER_API + '/' + subject + '/' + year).then(function(res){
+      this.setState({paper:res.data});
+    }.bind(this));
   }
 
   render() {
+    var { paper } = this.state;
       return(
-          <div>
-            {this.props.match.params.subject} - {this.props.match.params.year}
-          </div>
+        <div>
+          <Fade
+            in={paper !== ''}
+            style={{
+            transitionDelay: paper !== '' ? '800ms' : '0ms',
+            }}
+            unmountOnExit>
+              <div>
+                {paper.subject} - {paper.year}
+              </div>
+          </Fade>
+        </div>
       )
   }
 }
