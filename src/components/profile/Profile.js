1a8ea21caecd8a402cbd1edd040ce21b22d833d2
hirantha <sahanhirantha@gmail.com>
2019-04-22 22:46:45 +0530

    page added for profile and routed set

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+
+class Profile extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import {BrowserRouter, Route , Switch} from "react-router-dom";
-import { createStore,applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
@@ -17,6 +17,8 @@ import Papers from './components/papers/Papers';
 import Paper from './components/papers/Paper';
 import NewPaper from './components/papers/NewPaper';
 
+import Profile from './components/profile/Profile';
+
 import * as serviceWorker from './serviceWorker';
 import reducers from './reducers';
 import authGuard from './components/HOCs/AuthGuard';
@@ -26,30 +28,34 @@ const jwtToken = localStorage.getItem('JWT_TOKEN');
 axios.defaults.headers.common['Authorization'] = jwtToken;
 
 ReactDOM.render(
-    <Provider store={createStore(reducers,{
-        auth:{
-          token:jwtToken,
-          isAuthenticated:jwtToken ? true : false
+    <Provider store={createStore(reducers, {
+        auth: {
+            token: jwtToken,
+            isAuthenticated: jwtToken ? true : false
         }
-      },applyMiddleware(reduxThunk))}>
-      <BrowserRouter>
-          <App>
-            <Switch>
-              <Route exact path="/" component={signInUpGuard(Index)}/>
-              <Route exact path="/signup" component={signInUpGuard(SignUp)}/>
-              <Route exact path="/signin" component={signInUpGuard(SignIn)}/>
-              <Route exact path="/home" component={authGuard(Home)}/>
-              <Route exact path="/papers" component={authGuard(Papers)}/>
-              <Route exact path="/paper/:subject/:year" component={authGuard(Paper)}/>
-              <Route exact path="/papers/new" component={authGuard(NewPaper)}/>
-              <Route exact={true} path="*" component={NotFound}/>
-            </Switch>
-        </App>
-      </BrowserRouter>
+    }, applyMiddleware(reduxThunk))}>
+        <BrowserRouter>
+            <App>
+                <Switch>
+                    <Route exact path="/" component={signInUpGuard(Index)}/>
+                    <Route exact path="/signup" component={signInUpGuard(SignUp)}/>
+                    <Route exact path="/signin" component={signInUpGuard(SignIn)}/>
+                    <Route exact path="/home" component={authGuard(Home)}/>
+
+                    <Route exact path="/papers" component={authGuard(Papers)}/>
+                    <Route exact path="/paper/:subject/:year" component={authGuard(Paper)}/>
+                    <Route exact path="/papers/new" component={authGuard(NewPaper)}/>
+
+                    <Route exact path="/profile" component={authGuard(Profile)}/>
+
+                    <Route exact={true} path="*" component={NotFound}/>
+                </Switch>
+            </App>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
 
-    // <Route exact path="/dashboard" component={authGuard(Dashboard)}/>
+// <Route exact path="/dashboard" component={authGuard(Dashboard)}/>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
